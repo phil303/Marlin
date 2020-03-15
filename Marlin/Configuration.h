42f7c0f90ab80d59a25c63a5a4576adec4d836c0
Phil Aquilina <philaquilina@gmail.com>
2020-03-14 20:10:42 -0700

    more accurate xy offsets

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -932,7 +932,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -43, -10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -42.6, -11, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
